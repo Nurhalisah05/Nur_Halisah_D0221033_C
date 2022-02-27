421e6342986f91b40a72a8706d998fb654e8c66a
Nur halisah <99736470+Nurhalisah05@users.noreply.github.com>
2022-02-27 23:56:03 +0800

    komentar

---
+++ day9_And_Of_File.java.java
@@ -0,0 +1,20 @@
+
+package via;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class day9_And_Of_File_java {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int k =1;
+        while(sc.hasNext()){
+            String s = sc.nextLine();
+            System.out.println(k+" "+s);
+            k++;
+        }
+    }
+    
+}
