274f986c7ebdbc58433b809aec502235d3ea2115
Nur halisah <99736470+Nurhalisah05@users.noreply.github.com>
2022-03-17 21:47:45 +0800

    komentar

---
+++ Day27_java_biglnteger.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Solution {
+    //Nur halisah Day27 java biglnteger
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        BigInteger a = new BigInteger(scan.nextLine());
+        BigInteger b = new BigInteger(scan.nextLine());
+        System.out.println(a.add(b));
+        System.out.println(a.multiply(b));
+    }
+}
