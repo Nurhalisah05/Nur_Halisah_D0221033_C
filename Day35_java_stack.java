18d0f8237bdefd1c178419e1837dd7e9e119d653
Nur halisah <99736470+Nurhalisah05@users.noreply.github.com>
2022-03-25 19:54:35 +0800

    komentar

---
+++ Day35_java_stack.java
@@ -0,0 +1,39 @@
+import java.util.*;
+//Nur halisah Day35 java stack
+class Solution{
+   
+   public static void main(String []argh)
+   {
+      Scanner sc = new Scanner(System.in);
+      
+      while (sc.hasNext()) {
+         String input=sc.next();
+         System.out.println((balancedParenthensies(input)));
+      }
+      
+   }
+    
+     public static boolean balancedParenthensies(String s) {
+        Stack<Character> stack  = new Stack<Character>();
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(c == '[' || c == '(' || c == '{' ) {     
+                stack.push(c);
+            } else if(c == ']') {
+                if(stack.isEmpty() || stack.pop() != '[') {
+                    return false;
+                }
+            } else if(c == ')') {
+                if(stack.isEmpty() || stack.pop() != '(') {
+                    return false;
+                }           
+            } else if(c == '}') {
+                if(stack.isEmpty() || stack.pop() != '{') {
+                    return false;
+                }
+            }
+
+        }
+        return stack.isEmpty();
+    }
+}
