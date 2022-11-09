889b68e006b884547f88b5e581deeb5e773cdb7d
Nur halisah <99736470+Nurhalisah05@users.noreply.github.com>
2022-11-09 23:33:25 +0800

    Day24

---
+++ Day24.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package day2;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Day24 {
+    public static void main(String[] args) {
+
+        int num = 4;
+        for(int i = 1; i <= 10; ++i)
+        {
+            System.out.printf("%d * %d = %d \n", num, i, num * i);
+        }
+    }
+}
+    
