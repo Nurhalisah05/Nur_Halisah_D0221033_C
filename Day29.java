237308f77ab9d93bd7be862737459658088a1ecd
Nur halisah <99736470+Nurhalisah05@users.noreply.github.com>
2022-11-14 23:09:51 +0800

    oprator perbandingan

---
+++ Day29.java
@@ -0,0 +1,28 @@
+package day2;
+
+public class Day29 {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        boolean hasil;
+
+        hasil = a == b;
+        System.out.println("Apakah a == b ? " + hasil);
+
+        hasil = a != b;
+        System.out.println("Apakah a != b ? " + hasil);
+
+        hasil = a > b;
+        System.out.println("Apakah a > b ? " + hasil);
+
+        hasil = a < b;
+        System.out.println("Apakah a < b ? " + hasil);
+
+        hasil = a >= b;
+        System.out.println("Apakah a >= b ? " + hasil);
+
+        hasil = a <= b;
+        System.out.println("Apakah a <= b ? " + hasil);
+    }
+}
